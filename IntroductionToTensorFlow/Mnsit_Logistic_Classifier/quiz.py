113fc4987c5de08461046e97d6dcbcdd1219ec3f
ramsrigouthamg <ramsrigouthamg@gmail.com>
2017-01-02 20:26:20 -0800

    Adding tensorflow Minibatch Training

---
+++ IntroductionToTensorFlow/Mnsit_Logistic_Classifier/quiz.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  2 08:56:12 2017
+
+@author: ramsri
+"""
+
+# Quiz Solution
+# Note: You can't run code in this tab
+import tensorflow as tf
+
+def weights(n_features, n_labels):
+    """
+    Return TensorFlow weights
+    :param n_features: Number of features
+    :param n_labels: Number of labels
+    :return: TensorFlow weights
+    """
+    # TODO: Return weights
+    return tf.Variable(tf.truncated_normal((n_features, n_labels)))
+
+
+def biases(n_labels):
+    """
+    Return TensorFlow bias
+    :param n_labels: Number of labels
+    :return: TensorFlow bias
+    """
+    # TODO: Return biases
+    return tf.Variable(tf.zeros(n_labels))
+
+
+def linear(input, w, b):
+    """
+    Return linear function in TensorFlow
+    :param input: TensorFlow input
+    :param w: TensorFlow weights
+    :param b: TensorFlow biases
+    :return: TensorFlow linear function
+    """
+    # TODO: Linear Function (xW + b)
+    return tf.add(tf.matmul(input, w), b)
