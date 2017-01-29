e742025aac654ba40bc0120d0f03842818b3eaef
RAMSRIG\Ramsri <ramsrigouthamg@gmail.com>
2017-01-28 20:44:51 -0800

    Adding the code for driving car

---
+++ Behavioral_Cloning_Keras/drive.py
@@ -0,0 +1,106 @@
+import argparse
+import base64
+import json
+
+import numpy as np
+import socketio
+import eventlet
+import eventlet.wsgi
+import time
+from PIL import Image
+from PIL import ImageOps
+from flask import Flask, render_template
+from io import BytesIO
+
+from keras.models import model_from_json
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+import cv2
+
+# Fix error with Keras and TensorFlow
+import tensorflow as tf
+tf.python.control_flow_ops = tf
+
+
+sio = socketio.Server()
+app = Flask(__name__)
+model = None
+prev_image_array = None
+
+
+def crop_Image(image):
+    height, width = image.shape[:2]
+    # print (height,width)
+    #Remove a region from top and bottom of the image to remove sky and hood
+    upper_limit = int((6.0/7.0)*height)
+    lower_limit = int((2.0 / 7.0) * height)
+    return image[lower_limit:upper_limit,:]
+
+def preProcess(image):
+    image = crop_Image(image)
+    image= cv2.resize(image, (200, 66))
+    return image
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    # The current steering angle of the car
+    steering_angle = data["steering_angle"]
+    # The current throttle of the car
+    throttle = data["throttle"]
+    # The current speed of the car
+    speed = data["speed"]
+    # The current image from the center camera of the car
+    imgString = data["image"]
+    image = Image.open(BytesIO(base64.b64decode(imgString)))
+    image_array = np.asarray(image)
+    # print ("Entered Here... Initial shape of image captured")
+    # print (image_array.shape)
+    image_array = preProcess(image_array)
+    # print ("Entered Here... Final shape of image captured")
+    # print (image_array.shape)
+    transformed_image_array = image_array[None, :, :, :]
+    # This model currently assumes that the features of the model are just the images. Feel free to change this.
+    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+    # The driving model currently just outputs a constant throttle. Feel free to edit this.
+    throttle = 0.2
+    # steering_angle = 0.001
+    print(steering_angle, throttle)
+    send_control(steering_angle, throttle)
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print("connect ", sid)
+    send_control(0.0, 0.0)
+
+
+def send_control(steering_angle, throttle):
+    sio.emit("steer", data={
+    'steering_angle': steering_angle.__str__(),
+    'throttle': throttle.__str__()
+    }, skip_sid=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Remote Driving')
+    parser.add_argument('model', type=str,
+    help='Path to model definition json. Model weights should be on the same path.')
+    args = parser.parse_args()
+    with open(args.model, 'r') as jfile:
+        # NOTE: if you saved the file by calling json.dump(model.to_json(), ...)
+        # then you will have to call:
+        #
+        #   model = model_from_json(json.loads(jfile.read()))\
+        #
+        # instead.
+        model = model_from_json(jfile.read())
+
+
+    model.compile("adam", "mse")
+    weights_file = args.model.replace('json', 'h5')
+    model.load_weights(weights_file)
+
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
+
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
